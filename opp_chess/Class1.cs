5bf3af186578e213b6fe34895fe35b1515459728
David Čejka <Cejka.David@stud.vassboskovice.cz>
2022-03-29 09:17:51 +0200

    Přidejte soubory projektu.

---
+++ opp_chess/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opp_chess
+{
+    class Class1
+    {
+    }
+}
---
+++ opp_chess/figura.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opp_chess
+{
+    public class figura
+    {
+        public FigureType Type { get; set; }
+        public int MyProperty { get; set; }
+
+        public figura()
+        {
+            
+        }
+    }
+
+    //výčtový datový typ-seznam hodnot- interně jsou to hodnoty integer (0)
+    public enum FigureType
+    {
+        Pawn, //pěšec
+        Rook, //věž
+        Knight, //jezdec
+        King, //král
+        Gueen, //královna
+        Bishop, //střelec
+    }
+}
